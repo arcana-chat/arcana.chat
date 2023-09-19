5d63749a62aa5f90eb0c52395190fdbd35b2f440
Jay <social.jonathan@gmail.com>
2023-09-18 22:43:37 -0500

    python sandbox

--- packages/api/src/routes/ai.ts
+++ packages/api/src/routes/ai.ts
@@ -8,6 +8,9 @@ import { ChatSessionTable, MessageTable, UserTable } from '../db/schema';
 import { v4 as uuid } from 'uuid';
 import { createUser, getCurrentUser } from './user';
 
+type UserState = 'initial' | 'learning' | 'reading';
+let userState: UserState = 'initial';
+
 type Message = {
   role: 'user' | 'system';
   content: string;
@@ -16,15 +19,17 @@ type Message = {
 let messages: Message[] = [
   {
     role: 'system',
-    content: "Welcome to the Tarot Interpreter AI! Would you like a reading, or are you curious about specific cards?"
-  }
+    content:
+      'Welcome to the Tarot Interpreter AI! Would you like a reading, or are you curious about specific cards?',
+  },
 ];
 
 const guideUser = async (userResponse: string, ctx: any) => {
-  const context = "User wants to interact with a tarot reading AI. Understand if they want a reading or to learn about specific cards based on their response.";
+  const context =
+    "User wants to interact with a tarot reading AI. Please categorize their intention as either 'reading' or 'learning' based on their response.";
   const openaiMessages = [
     { role: 'system', content: context },
-    { role: 'user', content: userResponse }
+    { role: 'user', content: userResponse },
   ];
 
   try {
@@ -35,17 +40,59 @@ const guideUser = async (userResponse: string, ctx: any) => {
 
     const aiUnderstanding = completion.choices[0]?.message?.content;
 
-    if (aiUnderstanding.includes("interested in learning")) {
-      return "Which card are you curious about?";
-    } else if (aiUnderstanding.includes("seeking a tarot reading")) {
-      return "Great! Let's start with your past, present, future spread. Please tell me the card you've drawn for 'past'.";
-    } else {
-      return "I'm not entirely sure what you're looking for. Could you clarify if you'd like a reading or want to learn about a specific card?";
+    if (userState === 'initial') {
+      console.log(">>>>>>>>>Inside 'initial' state logic.<<<<<<<<<");
+      if (aiUnderstanding.trim().toLowerCase() === 'learning') {
+        userState = 'learning'; // Update the state
+        return 'Which card are you curious about?';
+      } else if (aiUnderstanding.trim().toLowerCase() === 'reading') {
+        userState = 'reading'; // Update the state
+        return "Great! Let's start with your past, present, future spread. Please tell me the card you've drawn for 'past'.";
+      } else {
+        return "I'm not entirely sure what you're looking for. Could you clarify if you'd like a reading or want to learn about a specific card?";
+      }
     }
 
+    // If the user's state is "learning"
+    else if (userState === 'learning') {
+      console.log(">>>>>>>>>Updated userState to:", userState, "<<<<<<<<<");
+      const context = `You are an ai powered tarot bot. The user has indicated that they want to learn about tarot cards. They have indicated that they want to learn about ${userResponse}. Can you provide a brief description about this card? The description should include the general meaning, the upright meaning, the reverse meaning, and any other associations.`;
+      const openaiMessages = [
+        { role: 'system', content: context },
+        { role: 'user', content: userResponse },
+      ];
+
+      try {
+        const completion = await ctx.openai.chat.completions.create({
+          model: 'gpt-4',
+          messages: openaiMessages,
+        });
+
+        const aiCardDescription = completion.choices[0]?.message?.content;
+
+        if (!aiCardDescription) {
+          throw new Error("Couldn't generate a response from the AI.");
+        }
+
+        return aiCardDescription;
+      } catch (error) {
+        console.error("Oops! The tarot cards didn't foresee this error: ", error);
+        return 'Hmm, something went wrong while reading the energies of that card. Can we try another?';
+      }
+    }
+
+    // If the user's state is "reading"
+    else if (userState === 'reading') {
+      console.log("Starting function. Current userState:", userState);
+      // Handle the reading logic here.
+      return `Let's discuss the significance of the ${userResponse} card in the 'past' position. ...`; // Interpret card for past.
+    } else {
+      console.error('Unexpected user state encountered.');
+      return "Oops! Something's not right on my end. Let's try to refocus.";
+    }
   } catch (error) {
-    console.error('Oh no! A wild error appeared: ', error);
-    return "Oops! Something went wrong on my end. Let's try that again.";
+    console.error("Seems like Mercury's in retrograde again, cause there was an error: ", error);
+    return "Oops! Something went awry on my end. Let's try that again.";
   }
 };
 
